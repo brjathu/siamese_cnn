a3cf46dc7359bcb7b7937e24a2e50ccf0dd91bbc
brjathu <brjathu@gmail.com>
2017-09-14 22:09:28 +1000

    edited code for icons search

--- search_data_set.py
+++ search_data_set.py
@@ -14,8 +14,7 @@ import matplotlib.image as mpimg
 import pickle
 from scipy.special import entr
 
-target_content = scipy.io.loadmat('playstore/features/content1000/orignal|angry_birds|angry_birds.mat')
-target_style = scipy.io.loadmat('playstore/features/style/orignal|angry_birds|angry_birds.mat')
+target_style = scipy.io.loadmat('test_data/icons/style/orignal|angry_birds|angry_birds.mat')
 search = True
 
 
@@ -24,27 +23,23 @@ def main():
     if(search == True):
 
         # first dirctory
-        path = "playstore/features"
+        path = "test_data/icons"
         count = 0
-        location = os.listdir(path + "/content1000/")
+        location = os.listdir(path + "/style/")
         for file in location:
-            name = file.split("|")
-            if(name[1] == "angry_birds"):
+            # name = file.split("|")
+            # if(name[1] == "angry_birds"):
+            style = scipy.io.loadmat(path + '/style/' + file)
 
-                content = scipy.io.loadmat(path + '/content1000/' + file)
-                style = scipy.io.loadmat(path + '/style/' + file)
+            sqr = score(style, 2)
+            if(count == 0):
+                table = np.array([file, cos[0], cos[1], sqr[0], sqr[1]])
+            else:
+                table = np.vstack((table, [file, cos[0], cos[1], sqr[0], sqr[1]]))
 
-                # print(sum_content, sum_style)
-                cos = score(style, content, 1)
-                sqr = score(style, content, 2)
-                if(count == 0):
-                    table = np.array([file, cos[0], cos[1], sqr[0], sqr[1]])
-                else:
-                    table = np.vstack((table, [file, cos[0], cos[1], sqr[0], sqr[1]]))
-
-                if(count % 100 == 0):
-                    print(count)
-                count = count + 1
+            if(count % 100 == 0):
+                print(count)
+            count = count + 1
 
         print("===========   DONE  ===========")
         print(table)
@@ -72,22 +67,12 @@ def main():
         # fig.savefig("me.png")
 
 
-def score(style, content, type=1):
+def score(style):
 
-    if(type == 1):
-        cosine_mat_style = (style['conv5_1'] * target_style['conv5_1'])
-        sum_style = np.sum(cosine_mat_style) / np.sqrt(np.sum(style['conv5_1']**2) * np.sum(target_style['conv5_1']**2))
+    squred_difference_style = (((style['conv5_1'] - target_style['conv5_1']))**2)
+    sum_style = np.sum(squred_difference_style)
 
-        cosine_mat_content = (content['prob'] * target_content['prob'])
-        sum_content = np.sum(cosine_mat_content) / np.sqrt(np.sum(content['prob']**2) * np.sum(target_content['prob']**2))
-    elif(type == 2):
-        squred_difference_style = (((style['conv5_1'] - target_style['conv5_1']))**2)
-        sum_style = np.sum(squred_difference_style)
-
-        squred_difference_content = (((content['prob'] - target_content['prob']))**2)
-        sum_content = np.sum(squred_difference_content)
-
-    return([sum_content, sum_style])
+    return(sum_style)
 
 
 if __name__ == '__main__':
