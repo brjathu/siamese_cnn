372da2e234cd0a70995147d94acb860e8733b35b
brjathu <brjathu@gmail.com>
2017-11-03 12:07:43 +1100

    updates

--- siamese_multi_gpu.py
+++ siamese_multi_gpu.py
@@ -1,26 +1,3 @@
-# import tensorflow as tf
-# import numpy as np
-
-# with tf.variable_scope("a"):
-#     s = tf.get_variable("test", [10], initializer=tf.constant_initializer(np.ones(10)))
-#     n = tf.get_variable("test2", [10], initializer=tf.constant_initializer(20))
-
-# with tf.variable_scope("a", reuse=True):
-#     b = tf.get_variable("test2", [10], initializer=tf.constant_initializer(15))
-
-
-# print(s)
-# print(b)
-
-# with tf.Session() as sess:
-#     sess.run(tf.global_variables_initializer())
-
-#     a = sess.run([s])
-#     print(a)
-
-#     a = sess.run([b])
-#     print(a)
-#     # print(s2)
 import os
 import numpy as np
 import tensorflow as tf
@@ -32,23 +9,37 @@ import math
 import re
 
 num_gpus = 3
+EPOCH = 100
+batch_size = 3
+model = "testP1"
+main_dir = "/flush1/raj034/vgg19/" + model + "/"
+# main_dir = "bck/" + model + "/"
+os.system("mkdir " + main_dir)
+LOG_FILE = open(main_dir + 'log.txt', 'a')
+
+
+train_data = np.load("data/final_train.npy", encoding='latin1')
+# logEntry("debugger a ========= >  " + str(train_data.shape))
+train_data = train_data[0:800, :]
+
+
+val_data = np.load("data/final_val.npy", encoding='latin1')
+# logEntry("debugger c ========= >  " + str(val_data.shape))
+val_data = val_data[0:160, :]
+
+test_data = np.load("data/final_test.npy", encoding='latin1')
+
+
+def logEntry(TMP_STRING):
+    LOG_FILE.write(str(TMP_STRING))
+    LOG_FILE.write("\n")
+    LOG_FILE.flush()
+    print(str(TMP_STRING))
 
 
 def tower_loss(scope, images1, images2, pos, neg):
-    """Calculate the total loss on a single tower running the CIFAR model.
-    Args:
-      scope: unique prefix string identifying the CIFAR tower, e.g. 'tower_0'
-      images: Images. 4D tensor of shape [batch_size, height, width, 3].
-      labels: Labels. 1D tensor of shape [batch_size].
-    Returns:
-       Tensor of shape [] containing the total loss for a batch of data
-    """
-    # Build inference Graph.
-    loss = siamese.inference(images1, images2, pos, neg)
 
-    # Build the portion of the Graph calculating the losses. Note that we will
-    # assemble the total_loss using a custom function below.
-    # _ = cifar10.loss(logits, labels)
+    loss = siamese.inference(images1, images2, pos, neg)
 
     # Assemble all of the losses for the current tower only.
     losses = tf.get_collection('losses', scope)
@@ -102,52 +93,6 @@ def average_gradients(tower_grads):
         average_grads.append(grad_and_var)
     return average_grads
 
-# main_dir = "/flush1/raj034/vgg19/" + model + "/"
-# os.system("mkdir " + main_dir)
-# LOG_FILE = open(main_dir + 'log.txt', 'a')
-
-train_data = np.load("data/final_train.npy", encoding='latin1')
-# logEntry("debugger a ========= >  " + str(train_data.shape))
-train_data = train_data[0:800, :]
-# logEntry("debugger b ========= >  " + str(train_data.shape))
-
-val_data = np.load("data/final_val.npy", encoding='latin1')
-# logEntry("debugger c ========= >  " + str(val_data.shape))
-val_data = val_data[0:160, :]
-# logEntry("debugger d ========= >  " + str(val_data.shape))
-
-test_data = np.load("data/final_test.npy", encoding='latin1')
-
-
-# loss = siamese.inference(images1, images2)
-# init = tf.global_variables_initializer()
-
-# with tf.device('/cpu'), tf.Session(config=tf.ConfigProto(allow_soft_placement=True, log_device_placement=True)) as sess:
-
-#         # feed_dict = {images: batch}
-
-#         # Start running operations on the Graph. allow_soft_placement must be set to
-#         # True to build towers on GPU, as some of the ops do not have GPU
-#         # implementations.
-#     sess.run(init)
-
-#     img1 = utils.load_image("cat.jpg")
-#     img2 = utils.load_image("dog.jpg")
-#     batch1 = img1.reshape((1, 224, 224, 3))
-#     batch2 = img2.reshape((1, 224, 224, 3))
-
-#     a = sess.run(loss, feed_dict={images1: batch1, images2: batch2})
-
-#     print(a)
-#     LOGDIR = 'LOG_test'
-#     train_writer = tf.summary.FileWriter(LOGDIR)
-#     train_writer.add_graph(sess.graph)
-#     train_writer.close()
-#     # # logEntry("debugger b ========= >  " + str(train_data[class_num, i]))
-
-#     #     for i in range(batch_size):
-#     #         features = np.reshape(conv5_1[i], (-1, 512))
-#     #         # logEntry("debugger
 
 siamese = sia.sia("vgg19.npy")
 with tf.Graph().as_default(), tf.device('/cpu:0'):
@@ -166,10 +111,10 @@ with tf.Graph().as_default(), tf.device('/cpu:0'):
 
     tower_grads = []
 
-    images1 = tf.placeholder(tf.float32, [3, 224, 224, 3], name="images1")
-    images2 = tf.placeholder(tf.float32, [3, 224, 224, 3], name="images2")
-    pos = tf.placeholder(tf.float32, [3, 1], name="positive_samples")
-    neg = tf.placeholder(tf.float32, [3, 1], name="negetive_samples")
+    images1 = tf.placeholder(tf.float32, [batch_size, 224, 224, 3], name="images1")
+    images2 = tf.placeholder(tf.float32, [batch_size, 224, 224, 3], name="images2")
+    pos = tf.placeholder(tf.float32, [batch_size, 1], name="positive_samples")
+    neg = tf.placeholder(tf.float32, [batch_size, 1], name="negetive_samples")
 
     split_img1 = tf.split(images1, num_gpus)
     split_img2 = tf.split(images2, num_gpus)
@@ -189,16 +134,16 @@ with tf.Graph().as_default(), tf.device('/cpu:0'):
 
     with tf.variable_scope("opts"):
         grads = average_gradients(tower_grads)
-        # summaries.append(tf.summary.scalar('learning_rate', lr))
+        summaries.append(tf.summary.scalar('learning_rate', lr))
 
-        # for grad, var in grads:
-        #     if grad is not None:
-        #         summaries.append(tf.summary.histogram(var.op.name + '/gradients', grad))
+        for grad, var in grads:
+            if grad is not None:
+                summaries.append(tf.summary.histogram(var.op.name + '/gradients', grad))
 
         apply_gradient_op = opt.apply_gradients(grads, global_step=global_step)
 
-        # for var in tf.trainable_variables():
-        #     summaries.append(tf.summary.histogram(var.op.name, var))
+        for var in tf.trainable_variables():
+            summaries.append(tf.summary.histogram(var.op.name, var))
 
         variable_averages = tf.train.ExponentialMovingAverage(
             0.9999, global_step)
@@ -206,9 +151,9 @@ with tf.Graph().as_default(), tf.device('/cpu:0'):
 
         train_op = tf.group(apply_gradient_op, variables_averages_op)
 
-    # saver = tf.train.Saver(tf.global_variables())
+    saver = tf.train.Saver(tf.global_variables())
 
-    # summary_op = tf.summary.merge(summaries)
+    summary_op = tf.summary.merge(summaries)
 
     init = tf.global_variables_initializer()
 
@@ -217,29 +162,39 @@ with tf.Graph().as_default(), tf.device('/cpu:0'):
         log_device_placement=True))
     sess.run(init)
 
-    LOGDIR = 'LOG_test'
-    train_writer = tf.summary.FileWriter(LOGDIR)
-    train_writer.add_graph(sess.graph)
-    train_writer.close()
-    # summary_writer = tf.summary.FileWriter("LOG_test", sess.graph)
+    loss_avg = 0
+    val_avg = 0
+    distibution = np.array((batch_size, 2))
+    validation = np.array((batch_size, 2))
 
-    # for step in range(1000):
-    #     start_time = time.time()
-    #     img1 = utils.load_image("cat.jpg")
-    #     img2 = utils.load_image("dog.jpg")
+    for epoch in range(EPOCH):
 
-    #     batch1 = img1.reshape((1, 224, 224, 3))
-    #     batch2 = img2.reshape((1, 224, 224, 3))
+        for mini_batch in range(int(train_data.shape[0] / batch_size)):
+            start_time = time.time()
+            min_batch_data = train_data[mini_batch * batch_size: (mini_batch + 1) * batch_size]
+            y_pos = np.reshape(train_batch_data[:, 3], (batch_size, 1))
+            y_neg = (y_pos - 1) * (-1)  # change
+            count = 0
 
-    #     batch11 = np.concatenate((batch1, batch1), 0)
-    #     batch11 = np.concatenate((batch11, batch1), 0)
+            for i in range(batch_size):
+                img1 = utils.load_image("/flush1/raj034/wikiart/wikiart/" + min_batch_data[i, 0][0])
+                img2 = utils.load_image("/flush1/raj034/wikiart/wikiart/" + min_batch_data[i, 0][1])
 
-    #     batch22 = np.concatenate((batch2, batch2), 0)
-    #     batch22 = np.concatenate((batch22, batch2), 0)
+                if count == 0:
+                    batch1 = img1.reshape((1, 224, 224, 3))
+                    batch2 = img2.reshape((1, 224, 224, 3))
+                else:
+                    batch1 = np.concatenate((batch1, img1.reshape((1, 224, 224, 3))), 0)
+                    batch2 = np.concatenate((batch2, img2.reshape((1, 224, 224, 3))), 0)
 
-    #     y_pos = np.reshape([1, 1, 1], (3, 1))
-    #     y_neg = (y_pos - 1) * (-1)
-    #     _, loss_value, lr_val = sess.run([train_op, loss, lr], feed_dict={images1: batch11, images2: batch22, pos: y_pos, neg: y_neg})
-    #     print(loss_value)
-    #     print(lr_val)
-    #     duration = time.time() - start_time
+                count += 1
+            _, loss_value, lr_val, summary_str = sess.run([train_op, loss, lr, summary_op], feed_dict={images1: batch1, images2: batch2, pos: y_pos, neg: y_neg})
+            logEntry("epoch = " + str(epoch) + "\t mini batch = " + str(mini_batch) + "\t loss =" loss_value)
+            logEntry(lr_val)
+            duration = time.time() - start_time
+            logEntry("time = " + str(duration))
+            summary_writer = tf.summary.FileWriter(main_dir + LOG_test)
+            summary_writer.add_graph(sess.graph)
+            summary_writer.add_summary(summary_str, epoch)
+
+    summary_writer.close()
